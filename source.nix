bbabbd7f42e4dec9db8cda63edad53b5545ffa5b
wattmto <dev@wattmto.dev>
2025-02-18 12:51:41 +0900

    chatgpt: 1.2025.014 -> 1.2025.036

---
+++ source.nix
@@ -0,0 +1,7 @@
+{
+  version = "1.2025.036";
+  src = fetchurl {
+    url = "https://persistent.oaistatic.com/sidekick/public/ChatGPT_Desktop_public_1.2025.036_1739293618.dmg";
+    sha256 = "sha256-+mPgn+3RFStp45BGrlafB5c854ZcaToF7+nzY6xVtco=";
+  };
+}
