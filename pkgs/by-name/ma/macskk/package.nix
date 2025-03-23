8e07a65c461eb243c3037564242d07d61d74e590
wattmto <93639059+wattmto@users.noreply.github.com>
2025-03-23 02:37:35 +0000

    macskk: 1.11.0 -> 1.11.1

--- pkgs/by-name/ma/macskk/package.nix
+++ pkgs/by-name/ma/macskk/package.nix
@@ -11,11 +11,11 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "macskk";
-  version = "1.11.0";
+  version = "1.11.1";
 
   src = fetchurl {
     url = "https://github.com/mtgto/macSKK/releases/download/${finalAttrs.version}/macSKK-${finalAttrs.version}.dmg";
-    hash = "sha256-CqtW6bfSuAo+9VRmRTgx0aKpBKBEDIxidOh7V5vD7ww=";
+    hash = "sha256-Yp4mNuq0dDtfaielUpnVkIOVOXwyZ6+93pxyYyIsacQ=";
   };
 
   nativeBuildInputs = [
