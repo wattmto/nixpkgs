ecae92a96968d176895ac251cc407b7647113b2b
wattmto <93639059+wattmto@users.noreply.github.com>
2025-05-06 02:41:19 +0000

    chatgpt: 1.2025.063 -> 1.2025.112

--- pkgs/by-name/ch/chatgpt/source.nix
+++ pkgs/by-name/ch/chatgpt/source.nix
@@ -1,7 +1,7 @@
 {
-  version = "1.2025.063";
+  version = "1.2025.112";
   src = {
-    url = "https://persistent.oaistatic.com/sidekick/public/ChatGPT_Desktop_public_1.2025.063_1741403068.dmg";
-    hash = "sha256-ZqQZtrN+t6C5BRCHh8iqA/e11EA2GCvVTcxAAjK7Vq0=";
+    url = "https://persistent.oaistatic.com/sidekick/public/ChatGPT_Desktop_public_1.2025.112_1745628785.dmg";
+    hash = "sha256-q8IPRHKeFB5sjeUXHVKcY5E8Z8RfHSXeuOmJxzO1SJ4=";
   };
 }
